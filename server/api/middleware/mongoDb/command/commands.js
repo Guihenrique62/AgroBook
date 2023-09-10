9dcb51f4bbc8cf30d6c3e6035b6892e710c77109
Dev - Jean 202202257141 <jeantng2016@gmail.com>
2023-09-10 03:01:34 -0300

    Criacao da API - Listar usuarios

--- server/api/middleware/mongoDb/command/commands.js
+++ server/api/middleware/mongoDb/command/commands.js
@@ -10,13 +10,13 @@ const createData = async (dataBase, collectionName, obj) => {
 
     // FUNCAO PARA CRIAR UM REGISTRO
     async function execute(dataBase, collectionName, obj) {
-        
+
         await client.connect(); // AGUARDA A CONEXAO COM O CLIENT
 
         const db = client.db(dataBase); // CRIA A CONECAO COM O BANCO
         const collection = db.collection(collectionName); // AGORA A CONEXAO COM A COLLECTION
         const insertData = await collection.insertOne(obj); // PARA FINALIZAR REALIZA A INSERÇÃO DE UM UNICO OBJETO
-        
+
         const objArray = {}; // CRIA UM OBJETO PARA GUARDAR O OBJ PASSADO
 
         objArray.dataBase = dataBase; // RESERVA OS DADOS DE ENTRADA [ db ]
@@ -30,16 +30,45 @@ const createData = async (dataBase, collectionName, obj) => {
 
     return await execute(dataBase, collectionName, obj)
         .then((res) => { return res }) // EM CADO DE SUCESSO
-        .catch((err) => {return err}) // EM CASO DE ERRO
+        .catch((err) => { return err }) // EM CASO DE ERRO
         .finally(() => client.close()); // AO FINALIZAR FECHA A CONEXAO
 
 }
 
 // R = READ | Função que cria um registro no mongoDb | Ex: [ db = 'books', collection = 'usuarios', obj = {nome: "user"} ]
-const readData = async () => {
+const readData = async (dataBase, collectionName, filter, sort, limit) => {
 
     const configParms = new configDB(); // RECUPERA A FUNCAO QUE VEIO DO AQUIVO DE CONEXAO
     const client = await configParms.parmsConfigDB(); // RESERVA APENAS A FUNCAO DE CONEXAO
+
+    // FUNCAO PARA LER REGISTROS
+    async function execute(dataBase, collectionName, filter, sortfIld, limitfild) {
+
+        await client.connect(); // AGUARDA A CONEXAO COM O CLIENT
+
+        const db = client.db(dataBase); // CRIA A CONECAO COM O BANCO
+        const collection = db.collection(collectionName); // AGORA A CONEXAO COM A COLLECTION
+        const findData = await collection.find(filter).sort(sortfIld).limit(limitfild).toArray(); // PARA FINALIZAR REALIZA A INSERÇÃO DE UM UNICO OBJETO
+
+        console.log(findData);
+
+        const objArray = {}; // CRIA UM OBJETO PARA GUARDAR O OBJ PASSADO
+
+        objArray.dataBase = dataBase; // RESERVA OS DADOS DE ENTRADA [ db ]
+        objArray.collectionName = collectionName; // RESERVA OS DADOS DE ENTRADA [ collection ]
+        objArray.filter = filter; // PARAMETRO USANDO PARA FILTRAR QUERY
+        objArray.sortfIld = sortfIld; // PARAMETRO USANDO PARA ORDENAR QUERY
+        objArray.limitfild = limitfild; // PARAMETRO USANDO PARA LIMITAR NUMEROS DE REGISTRO QUERY
+        objArray.result = findData; // RESERVA O RESULTADO FINAL DO COMANDO
+
+        return objArray; // RETORNA O RESULTADO DA OPERAÇÃO O ESPERADO É {"ok":1}
+
+    }
+
+    return await execute(dataBase, collectionName, filter, sort, limit)
+        .then((res) => { return res }) // EM CADO DE SUCESSO
+        .catch((err) => { return err }) // EM CASO DE ERRO
+        .finally(() => client.close()); // AO FINALIZAR FECHA A CONEXAO
 
 }
 
@@ -56,6 +85,11 @@ module.exports = function () {
 
     this.commandCreateData = async (db, collection, obj) => {
         const data = await createData(db, collection, obj);
+        return data;
+    }
+
+    this.commandReadData = async (dataBase, collectionName, filter, sort, limit) => {
+        const data = await readData(dataBase, collectionName, filter, sort, limit);
         return data;
     }
 
--- server/api/router/user/read_user.js
+++ server/api/router/user/read_user.js
@@ -6,17 +6,35 @@ EMAIL: jeantng2016@gmail.com
 
 const express = require("express"); // EXTRAI O MODULO DO EXPRESS
 var router = express.Router(); // EXTRAR O MODULO DE ROTAS
+const commands = require('../../middleware/mongoDb/command/commands'); // EXTRAR OS COMANDOS NO MONGODB
 require('dotenv').config(); // SOLICITA AS VARIAVEIS DE AMBIENTE
 
 // *************** GET ***************
 // Controla todas as rotas de listagem e login de usuario
 router.get("/api/user/list_user", async (req, res) => {
 
+    // VERIFICA SE O BODY ESTA VAZIO
+    if (Object.keys(req.body).length === 0) {
+
+        res.status(401).json({
+            "codigo": process.env.CODE_FAIL,
+            "resposta": process.env.MSG_SUCCESS_FAIL,
+            "mensagem":"Nenhum valor foi recebido pelo BODY, revise sua solicitacao e tente novamente",
+            "data_base": ""
+        });
+        return true;
+
+    }
+
+    const { filter, sort, limit } = req.body;
+    const shell_commands = new commands(); // CRIA UM CONSTRUTOR
+    const listUser = await shell_commands.commandReadData(`books`, `usuarios`, filter, sort, limit); // EXECULTA A FUNCAO QUE LER REGISTRO NO BANCO DE DADOS 
+
     res.status(200).json({
         "codigo": process.env.CODE_SUCCESS,
         "resposta": process.env.MSG_SUCCESS,
-        "mensagem":"Boas-vindas!",
-        "data_base": ""
+        "mensagem":"Lista de usuarios recuperada com sucesso",
+        "data_base": listUser
     });
 
 });
