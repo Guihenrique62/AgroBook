4af9a2529f5c6f95c4ec13bf09abe377095c6ce9
ihuryferreira <ihuryferreira@icloud.com>
2023-09-13 19:30:48 -0300

    Adicionamento do readme

--- server/api/main.js
+++ server/api/main.js
@@ -1,8 +1,8 @@
-const mainAppUse = require('./config/express');
-const app = new mainAppUse;
-const fs = require('fs');
-const https = require('https');
+const mainAppUse = require("./config/express");
+const app = new mainAppUse();
+const fs = require("fs");
+const https = require("https");
 
 /* ROUTER AUTH - Controla todas as entradas */
-var ROUTER_AUTH = require('./router/auth');
+var ROUTER_AUTH = require("./router/auth");
 app.use(ROUTER_AUTH);
--- server/api/router/auth.js
+++ server/api/router/auth.js
@@ -2,31 +2,48 @@ const express = require("express");
 var router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const md5 = require('md5');
-const check_user = require('../middleware/jwt/jwt');
-const user = require('../middleware/mongoDb/command/commands');
-require('dotenv').config()
+const md5 = require("md5");
+const check_user = require("../middleware/jwt/jwt");
+const commands = require("../middleware/mongoDb/command/commands");
+require("dotenv").config();
+
+const a = new commands();
+const b = a.commandCreateData();
 
 // *************** GET ***************
 // Controla todas as entradas global no metodo GET
-router.get("/*", async (req, res) => {
-
-    res.send({
-        "status": "ok",
-        "router": "global"
-    });
-
-});
+// router.get("/*", async (req, res) => {
+//     res.send({
+//         status: "ok",
+//         router: "global",
+//     });
+// });
 
 // *************** GET ***************
 // Controla todas as entrada de api no metodo GET
-router.get("/api/*", async (req, res) => {
+// a.router.get("/api/*", async (req, res) => {
+//     res.send({
+//         status: "ok",
+//         router: "api",
+//     });
+// });
+router.get("/", async (req, res, next) => {
+    res.status(200).send({
+        testando: [
+            {
+                id: 1,
+                nome: "ihury",
+                idade: 31,
+                descript: "Teste Concluido.",
+                stats: true,
+            },
+        ],
+    });
+    next();
+});
 
-    res.send({
-        "status": "ok",
-        "router": "api"
-    })
-
+router.get("/testx", async (req, res) => {
+    res.status(200).json({ stats: "Ativado", title: "Api Ativado" });
 });
 
 module.exports = router;
