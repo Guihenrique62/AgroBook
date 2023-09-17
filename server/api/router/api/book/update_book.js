8ec51e64c821717bdc0e8431dce851f6afe8190f
Hevaldo <camaly_dabike@hotmail.com>
2023-09-16 21:58:35 -0300

    Correção gramatical-update_book.js

--- server/api/router/api/book/update_book.js
+++ server/api/router/api/book/update_book.js
@@ -5,18 +5,18 @@ EMAIL: guilhermeportosantos1@gmail.com
 */
 
 const express = require("express"); // EXTRAI O MODULO DO EXPRESS
-var router = express.Router(); // EXTRAR O MODULO DE ROTAS
-const commands = require('../../../middleware/mongoDb/command/commands'); // EXTRAR OS COMANDOS NO MONGODB
-require('dotenv').config(); // SOLICITA AS VARIAVEIS DE AMBIENTE
+var router = express.Router(); // EXTRAI O MODULO DE ROTAS
+const commands = require('../../../middleware/mongoDb/command/commands'); // EXTRAI OS COMANDOS NO MONGODB
+require('dotenv').config(); // SOLICITA AS VARIÁVEIS DE AMBIENTE
 
 // *************** POST ***************
 // Controla todas as rotas de listagem e login de livros
 router.post("/api/book/update_book", async (req, res) => {
 
     const { filter, newValue } = req.body; // RESERVA VALORES DO BODY
-    const attrValid = ["titulo"]; // PERMITE APENAS ESSAS CHAVES COMO PARAMETROS ACEITAVES PARA ATUALIZAR LIVROS
+    const attrValid = ["titulo"]; // PERMITE APENAS ESSAS CHAVES COMO PARÂMETROS ACEITAVEIS PARA ATUALIZAR LIVROS
 
-    // VERIFICA SE A VARIAVEL FILTER ESTÁ VAZIA
+    // VERIFICA SE A VARIÁVEL FILTER ESTÁ VAZIA
     if (!filter || Object.keys(filter).length === 0 || typeof (filter) !== `object`) {
 
         res.status(401).json({
@@ -29,11 +29,11 @@ router.post("/api/book/update_book", async (req, res) => {
 
     }
 
-    //VERIFICA SE O FILTER É VALIDO
+    //VERIFICA SE O FILTER É VÁLIDO
     for (key in Object.keys(filter)) {
 
         var valor = Object.keys(filter)[key];
-        if (attrValid.indexOf(valor) == -1) { // VERIFICA SE O FILTER RECEBIDO ESTA DE ACORDO COM AS VARIAVEIS VALIDAS
+        if (attrValid.indexOf(valor) == -1) { // VERIFICA SE O FILTER RECEBIDO ESTÁ DE ACORDO COM AS VARIÁVEIS VÁLIDAS
             res.status(401).json({
                 "codigo": process.env.CODE_FAIL,
                 "resposta": process.env.MSG_SUCCESS_FAIL,
@@ -57,7 +57,7 @@ router.post("/api/book/update_book", async (req, res) => {
 
     }
 
-    // VERIFICA SE FOI SOLICITADO A ALTERACAO DO ID
+    // VERIFICA SE FOI SOLICITADO A ALTERAÇÃO DO ID
     if (newValue["_id"] || newValue["id"]) {
 
         res.status(401).json({
@@ -71,9 +71,9 @@ router.post("/api/book/update_book", async (req, res) => {
     }
 
     const shell_commands = new commands(); // CRIA O CONSTRUTOR
-    const updateBook = await shell_commands.commandUpadateData('books', 'livros', filter, newValue); // INICIAR A FUNCAO ATUALIZAR REGISTRO NO MONGO DB
+    const updateBook = await shell_commands.commandUpadateData('books', 'livros', filter, newValue); // INICIAR A FUNÇÃO ATUALIZA REGISTRO NO MONGO DB
 
-    // VERIFICA EXISTE VALORES DUPLICADOS
+    // VERIFICA SE EXISTE VALORES DUPLICADOS
     if (updateBook["keyValue"]) {
 
         res.status(401).json({
@@ -115,7 +115,7 @@ router.all("/api/user/update_book*", async (req, res) => {
     res.status(404).json({
         "codigo": process.env.CODE_FAIL,
         "resposta": process.env.MSG_SUCCESS_FAIL,
-        "mensagem": `O linkk expirou ou não existe, experimente acessar a documentacao da API em ${process.env.HOST_API_DOC}/doc/update_user`,
+        "mensagem": `O linkk expirou ou não existe, experimente acessar a documentação da API em ${process.env.HOST_API_DOC}/doc/update_user`,
         "data_base": ""
     });
 
