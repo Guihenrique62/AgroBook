ef1aab6848cf1530bce60df93a6799bc12d6c7d6
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-09-20 11:21:03 -0300

    correção de bug - Menu Lateral

--- client/src/Componentes/cadastro_usuarios/RegisterUser.jsx
+++ client/src/Componentes/cadastro_usuarios/RegisterUser.jsx
@@ -5,7 +5,20 @@ export default function RegisterUser() {
   return (
     <div>
       <div>
-        <MenuLateral/>
+        <MenuLateral actualPath={'/registerUser'} user={{
+  "_id": {
+    "$oid": "650353c79ba693038e773576"
+  },
+  "email": "jeantng2016@gmail.com",
+  "senha": "0f3f2c85a67b613214b4c95066f622b9",
+  "cargo": 0,
+  "status": 1,
+  "documento": {
+    "$numberLong": "70460827154"
+  },
+  "nome": "JEAN RODRIGUES 3",
+  "resetar_senha": 0
+}}/>
       </div>
     </div>
   )
