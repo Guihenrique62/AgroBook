22cd99a76cc20303c34c6588b450eeebdfa153c6
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-09-20 11:14:25 -0300

    Menu lateral - Logica

---
+++ client/src/Componentes/menu_lateral/MenuLateral.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import extractInitials from './scripts/extractInitials';
+
+export default function MenuLateral({ actualPath }, user) {
+  
+  if(!actualPath){ // Caso não receba o path
+    console.error('Path não passado para o menu lateral')
+  }
+
+  const InitialsName = extractInitials(user.name); //retorna as iniciais do Nome
+  
+  return (
+    <div>
+      <Link to={actualPath}><img src="/x-icon.png" alt="close-icon" /></Link> {/*Fechar modal redirecionando para o path atual em q se encontra */}
+      <div className="perfilContainer"><p>{InitialsName}</p></div>
+      <p>{user.name}</p> {/*Nome do Usuário */}
+
+      {/*LINK PARA A TELA INICIAL*/}
+      <Link to={'/menu'}>
+        <img src='/home-icon.png' alt='home-icon' />
+        <p>Tela Inicial</p>
+      </Link>
+
+      {/*LINK PARA A TELA DE CADASTRO*/}
+      {
+        user.cargo == 0 ? //verifica se o user é qualificado 
+          <Link to={'/registerUser'}>
+            <img src='/cadastro-icon.png' alt='cadastro-icon' />
+            <p>Cadastro</p>
+          </Link>
+          : null
+      }
+
+      {/*LINK PARA A TELA ESTOQUE*/}
+      {
+      user.cargo == 0 ? //verifica se o user é qualificado 
+        <Link to={'/'}>
+          <img src='/livros-icon.png' alt='livros-icon' />
+          <p>Estoque</p>
+        </Link> 
+      : null
+      }
+
+      {/*LINK PARA A TELA MINHA BIBLIOTECA*/}
+      <Link to={'/'}>
+        <img src='/livro-icon.png' alt='livro-icon' />
+        <p>Minha Biblioteca</p>
+      </Link>
+
+      {/*LINK PARA A TELA PEDIDOS*/}
+      <Link to={'/'}>
+        <img src='/pedidos-icon.png' alt='pedidos-icon' />
+        <p>Pedidos</p>
+      </Link>
+
+      <div className="logoContainer">
+        <img src="/logo-image.png" alt="logo-image" />
+      </div>
+    </div>
+  )
+}
