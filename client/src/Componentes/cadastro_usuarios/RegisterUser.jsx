03ba1a94be44263aa9031ed3cf498c5face010d6
Guilherme Henrique <103789281+Guihenrique62@users.noreply.github.com>
2023-09-21 16:31:50 -0300

    correcao

--- client/src/Componentes/cadastro_usuarios/RegisterUser.jsx
+++ client/src/Componentes/cadastro_usuarios/RegisterUser.jsx
@@ -2,21 +2,15 @@ import React from 'react'
 import Header from '../header/Header'
 import listarUsuario from '../../controllers/listUser'
 
-const user = listarUsuario()
-console.log(user)
+// const result = listarUsuario()
+// const users = result.data.result
+// console.log(users)
+
+const users = [{nome: 'Guilherme Henrique', cargo: 0, id: 1},{nome: 'Pedro Henrique', cargo: 0, id: 2},{nome: 'Johnata Henrique', cargo: 0, id: 3}]
 
 export default function RegisterUser() {
 
-
-  //Valida o Cargo do Colaborador e converte para string
-  var cargo
-  if(user.cargo){
-    const cargoMap = {0: 'Admin', 1: 'Colaborador'}
-    cargo = cargoMap[user.cargo]
-  }else{
-    cargo = "undefined"
-  }
-
+  
   return (
     <div>
       <div>
@@ -25,7 +19,9 @@ export default function RegisterUser() {
             <input type="text" placeholder='Buscar Livros...' />
             <button><i className='bx bx-search'></i></button>
         </div>
-        <button>Novo</button>
+        <Link to='/registerUser/new'><button>Novo</button></Link>
+
+
         <div className="usersContainer">
           <div className="headTable">
             <span>Nome:</span>
@@ -36,23 +32,16 @@ export default function RegisterUser() {
         
           <div className='userTable'>
 
-            <div className="user">
-              <p>{user.nome}</p>
-              <p>{user.email}</p>
-              <p>{cargo}</p>
-              <button >
-                <img src="edit-user-icon.png" alt="edit-user-icon" />
-              </button>
-            </div>
-
-            <div className="user">
-              <p>{user.nome}</p>
-              <p>{user.email}</p>
-              <p>{cargo}</p>
-              <button >
-                <img src="edit-user-icon.png" alt="edit-user-icon" />
-              </button>
-            </div>
+              {users.map((user)=> (
+                <div className="user" key={user.id}>
+                  <p>{user.nome}</p>
+                  <p>{user.email}</p>
+                  <p>{user.cargo == 0 ? 'Admin': 'Colaborador'}</p>
+                  <button >
+                    <img src="edit-user-icon.png" alt="edit-user-icon" />
+                  </button>
+              </div>
+              ))}
 
           </div>
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider, Router } from "react-router-dom";
+
+//paginas
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Tela_de_Login />,
+      },
+      {
+        path: "/validar",
+        element: <Validar ok={undefined} />,
+      },
+      {
+        path: "/test",
+        element: < />,
+      },
+      {
+        path: "/home",
+        element: <Home/>,
+      },
+      {
+        path:'/registerUser',
+        element: <RegisterUser/>,
+        children: 
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
