5033531d0b7f629747f8902825a7e0e53c8605fe
Victor_Rafaelgs <victorrafaelgs13@gmail.com>
2023-10-06 14:16:23 -0300

    login comi o cu de quem ta lendo

--- client/src/views/Tela_Login/Components/Tela_Login.jsx
+++ client/src/views/Tela_Login/Components/Tela_Login.jsx
@@ -43,32 +43,36 @@ export default function Tela_Login() {
     <div>
       <form onSubmit={handleSubmit} className={style.loginContainer}>
         <img className={style.logo} src={loginLogo} alt="" />
-        <label className="Email" htmlFor="">
-          <p>Email:</p>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            required
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-        </label>
-        <label htmlFor="senha">
-          <p>Senha:</p>
-          <input
-            type="password"
-            name="senha"
-            id="senha"
-            minLength="8"
-            maxLength="100"
-            // pattern="[A-Z a-z]+"
-            value={senha}
-            required
-            onChange={(ev) => setSenha(ev.target.value)}
-          />
-        </label>
-        <button type="subimt">Logar</button>
+        <div className={style.inputbox}>
+          <label className="Email" htmlFor="" className={style.label}>
+            <p>Email:</p>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              required
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+          </label>
+        </div>
+        <div className={style.inputbox}>
+          <label htmlFor="senha" className={style.label}>
+            <p>Senha:</p>
+            <input
+              type="password"
+              name="senha"
+              id="senha"
+              minLength="8"
+              maxLength="100"
+              // pattern="[A-Z a-z]+"
+              value={senha}
+              required
+              onChange={(ev) => setSenha(ev.target.value)}
+            />
+          </label>
+        </div>
+        <button type="subimt" className={style.logbutton}>Logar</button>
 
         <div>
           {status === 200 || 0 || "200" ? mensgaemRetornada : mensgaemRetornada}
