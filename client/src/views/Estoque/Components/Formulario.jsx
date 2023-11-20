358408cbf4782beace502a885475d8585bd781c6
Fillype_da_Silva <fillypecunha@gmail.com>
2023-11-19 22:43:24 -0300

    pedido

--- client/src/views/Estoque/Components/Formulario.jsx
+++ client/src/views/Estoque/Components/Formulario.jsx
@@ -180,6 +180,7 @@ export default function Formulario({addBook}){
                     {quantidade}
                     {pag}
                     {data_lancamento}
+              
                   </h2>
                  </div>
                  </form>
